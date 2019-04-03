64d9bc01d7810773c4dc1e7407ed5d355aa0a381
Wesley Santos <wesley.santos.silva@ccc.ufcg.edu.br>
2019-04-03 16:09:06 -0300

    Update oantagonista.py

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import scrapy
 import json
 
@@ -9,7 +10,7 @@ from ri_lab_01.items import RiLab01CommentItem
 class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
-    start_urls = []
+    start_urls = ['oantagonista": "https://www.oantagonista.com/']
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -18,9 +19,17 @@ class OantagonistaSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+        for article in response.css('article'):
+            yield{
+                'titulo': article.css('div.container-post-home a.article_link::attr("title")').get(),
+                #'subtitulo':
+                #'autor':
+                'data': article.css('div.container-post-home a.article_link span.postmeta time::text').get(),
+                'secao': article.css('div.container-post-home a.article_link span.postmeta span.categoria::text').get(),
+                'texto': article.css('div.container-post-home a.article_link p::text').get(),
+                'url': article.css('div.container-post-home a.article_link::attr("href")').get(),
+
+            }
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
